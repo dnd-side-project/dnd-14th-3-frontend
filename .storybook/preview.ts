a324afe7a4a6b8db096a64994d3e152a7aaba211
Wide-Pants <ajtwoddl1236@naver.com>
2026-02-03 19:47:12 +0900

    chore: Storybook MSW 연동

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,14 @@
 import type { Preview } from "@storybook/react-vite";
+import { initialize, mswLoader } from "msw-storybook-addon";
 
 import "../src/index.css";
 
+import { handlers } from "../src/mocks/handlers";
+
+initialize(undefined, handlers);
+
 const preview: Preview = {
+  loaders: [mswLoader],
   parameters: {
     controls: {
       matchers: {
