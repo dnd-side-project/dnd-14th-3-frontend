3cb6b75b4666f352d0dc59c9597ab83d40484406
Wide-Pants <ajtwoddl1236@naver.com>
2026-02-09 02:41:13 +0900

    feat: 임시 로딩 인디케이터

---
+++ src/components/shared/loading/LoadingIndicator.tsx
@@ -0,0 +1,9 @@
+export default function LoadingIndicator() {
+  return (
+    <div className="flex items-center justify-center rounded-md bg-gray-50 px-4 py-3">
+      <span className="text-base font-bold leading-normal tracking-[0.57px] text-gray-900">
+        Loading
+      </span>
+    </div>
+  );
+}
