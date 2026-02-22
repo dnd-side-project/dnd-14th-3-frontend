395c4da8d9e3ab888add873131cbc390073c8687
Wide-Pants <ajtwoddl1236@naver.com>
2026-02-23 02:31:15 +0900

    feat: OnboardingPage에 Widgets 통합

--- src/pages/OnboardingPage.tsx
+++ src/pages/OnboardingPage.tsx
@@ -1,3 +1,45 @@
+import { useCallback, useMemo } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+import type { OnboardingStep } from "@/types/on-board";
+
+import { getOnboardingStep } from "@/lib/on-board/getOnboardingStep";
+
+import { IntroStepWidget, ProfileSetupWidget } from "@/components/on-board";
+import NotificationPermissionWidget from "@/components/on-board/notification-permission/NotificationPermissionWidget";
+
 export default function OnboardingPage() {
-  return <div>온보딩페이지</div>;
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const stepParam = searchParams.get("step");
+  const step = getOnboardingStep(stepParam);
+
+  const handleChangeStep = useCallback((nextStep: OnboardingStep) => {
+    setSearchParams({ step: nextStep });
+  }, [setSearchParams]);
+
+  const handleOnboardingComplete = useCallback(() => {
+    navigate("/");
+  }, [navigate]);
+
+  const handleStepComplete = useMemo(() => ({
+    intro: () => handleChangeStep("profile"),
+    profile: () => handleChangeStep("notification"),
+    notification: () => handleOnboardingComplete(),
+  }), [handleChangeStep, handleOnboardingComplete]);
+
+  const renderOnboardingStep = () => {
+    switch (step) {
+      case "intro":
+      default:
+        return <IntroStepWidget onComplete={handleStepComplete.intro} />;
+      case "profile":
+        return <ProfileSetupWidget onComplete={handleStepComplete.profile} />;
+      case "notification":
+        return <NotificationPermissionWidget onComplete={handleStepComplete.notification} />
+    }
+  };
+
+  return renderOnboardingStep();
 }
