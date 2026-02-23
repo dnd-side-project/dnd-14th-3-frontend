46efcbfafc4a0fb5360dcb2ad82b59c969866533
Wide-Pants <ajtwoddl1236@naver.com>
2026-02-24 05:08:19 +0900

    fix: api/user export 순서 변경

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,5 +1,5 @@
 export type { PatchUserConsentsRequest, UserConsentResponse } from "./consent.type";
-export type { UserProfileResponse } from "./userProfile.type";
 export { getUserConsentsApi } from "./getUserConsents.api";
 export { getUserProfileApi } from "./getUserProfile.api";
 export { patchUserConsentsApi } from "./patchUserConsents.api";
+export type { UserProfileResponse } from "./userProfile.type";
